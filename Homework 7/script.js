9be3b89d0f205069b9af1322b2badff6c926f1b2
Alex Dunbar <alexandrea.dunbar92@gmail.com>
2026-08-30 21:52:01 -0700

    Add functionality to manage interests and update button styles

--- Homework 7/script.js
+++ Homework 7/script.js
@@ -2,9 +2,29 @@
 const formInputs = document.querySelectorAll("input");
 
 formInputs.forEach((input) => {
-
 input.addEventListener("input", () => {
     input.classList.toggle("valid-input");    
 });
 });
 
+const submitButton = document.querySelectorAll("button");
+
+submitButton.forEach((button) => {
+button.addEventListener("click", () => {
+    button.textContent= "Clicked!";   
+});
+});
+
+document.querySelector("#add-item").addEventListener("click", ()=> {
+    let text= document.querySelector("#add").value; 
+    let li = document.createElement("li"); 
+    li.textContent =text; 
+    document.querySelector(".favorites").appendChild(li);
+});
+
+document.querySelector("#del-item").addEventListener("click", ()=> {
+    let lastItem = document.querySelector(".favorites li:last-child"); 
+if (lastItem) {
+    lastItem.remove();
+}
+});
