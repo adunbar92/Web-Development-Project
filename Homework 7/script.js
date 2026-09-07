51ef5f15071b33f07cf81427d25cf3a90f937bb9
Alex Dunbar <alexandrea.dunbar92@gmail.com>
2026-09-06 23:42:41 -0700

    Refactor contact forms in index and photo gallery HTML files for improved structure and validation; enhance JavaScript for form validation and success messaging; update CSS for error and success styling.

--- Homework 7/script.js
+++ Homework 7/script.js
@@ -1,10 +1,53 @@
+const contactForm = document.querySelector("#contact")
+const firstName = document.querySelector("#first-name")
+const firstNameEmpty = document.querySelector("#first-name-empty")
+const lastName = document.querySelector("#last-name")
+const lastNameEmpty = document.querySelector("#last-name-empty")
+const email = document.querySelector("#email")
+const emailInvalid = document.querySelector("#email-invalid")
+const message = document.querySelector("#message")
+const messageEmpty = document.querySelector("#message-empty")
+const submissionSuccess = document.querySelector("#submission-success")
 
-const formInputs = document.querySelectorAll("input");
 
-formInputs.forEach((input) => {
-input.addEventListener("input", () => {
-    input.classList.toggle("valid-input");    
-});
+const validateForm = (inputBox, errorLabel, message) => {
+   if(inputBox.value.trim() === "") {
+errorLabel.textContent = message;
+    return false;
+   }else{
+errorLabel.textContent = "";
+    return true;
+   }
+}
+
+const validateEmail = () =>{
+    if(email.value.trim() === "") {
+    emailInvalid.textContent = "Email is required.";
+    return false;
+   }
+   if(!email.value.includes("@")){
+    emailInvalid.textContent = "Email is invalid.";
+    return false;
+   }
+
+   emailInvalid.textContent = "";
+   return true;
+}
+
+contactForm.addEventListener("submit", (event) => { 
+    event.preventDefault();
+    const isFirstNameValid =validateForm(firstName,firstNameEmpty,"First Name is required.");
+    const isLastNameValid = validateForm(lastName,lastNameEmpty,"Last Name is required.");
+    const isMessageValid = validateForm(message,messageEmpty,"Message is required.");
+    const isEmailValid = validateEmail();
+    
+    if(isFirstNameValid && isLastNameValid && isEmailValid && isMessageValid) {
+        submissionSuccess.textContent = "Successful!";
+        contactForm.reset();
+    }else{
+        submissionSuccess.textContent = "";
+    }
+
 });
 
 const submitButtons = document.querySelectorAll("button");
@@ -14,6 +57,35 @@ button.addEventListener("click", () => {
     button.textContent= "Clicked!";   
 });
 });
+
+const inputFields = document.querySelectorAll("input");
+const textareas = document.querySelectorAll("textarea");
+
+inputFields.forEach((input) => {
+input.addEventListener("input", () => {
+    if(input.value.trim() !== "") {
+        input.classList.remove("failure");
+        input.classList.add("success");
+}else{
+        input.classList.remove("success");
+        input.classList.add("failure");
+    }
+});
+});
+
+
+textareas.forEach((textarea) => {
+textarea.addEventListener("input", () => {
+    if(textarea.value.trim() !== "") {
+        textarea.classList.remove("failure");
+        textarea.classList.add("success");
+    }else{
+        textarea.classList.remove("success");
+        textarea.classList.add("failure");
+    }
+});
+});
+
 
 document.querySelector("#add-item").addEventListener("click", ()=> {
     let text= document.querySelector("#add").value; 
